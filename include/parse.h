82067701e3630238310a951b644d00e30aa71da8
Jeremias Da Silva Gomes <jerda-si@c3r8p3.42sp.org.br>
2025-02-19 18:29:38 -0300

    Update_parse

--- include/parse.h
+++ include/parse.h
@@ -3,16 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   parse.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jeremias <jeremias@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jerda-si <jerda-si@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/27 19:22:54 by jerda-si          #+#    #+#             */
-/*   Updated: 2025/02/13 16:51:36 by jeremias         ###   ########.fr       */
+/*   Updated: 2025/02/19 13:29:24 by jerda-si         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PARSE_H
-
 # define PARSE_H
+
+typedef struct s_redirection t_redirection;
 
 typedef struct s_cmd_node
 {
@@ -35,11 +36,6 @@ typedef struct s_cmd_list
 } t_cmd_list;
 
 t_cmd_tree	*parse_input(char *input_line);
-static int	process_token_expansions(t_token *tokens);
-static int	validate_command_syntax(t_cmd_list *cmd);
-static t_cmd_node	*create_command_node(t_cmd_list *cmd);
-static t_cmd_tree	*build_command_tree(t_cmd_list *commands);
-static int	validate_all_commands(t_cmd_list *commands);
 t_cmd_list *create_cmd_list_node(void);
 void add_token_to_cmd(t_cmd_list *list, t_token *token);
 t_cmd_list *split_by_pipes(t_token *tokens);
--- src/main.c
+++ src/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jeremias <jeremias@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jerda-si <jerda-si@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/13 15:16:29 by jerda-si          #+#    #+#             */
-/*   Updated: 2025/01/30 18:52:38 by jeremias         ###   ########.fr       */
+/*   Updated: 2025/02/19 18:28:21 by jerda-si         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ int main(void)
     
     while (1)
     {
-        input = readline("T&BShell% ");
+        input = readline("\x1B[32m🐗TBShell\x1B[37m% ");
         if (!input)
         {
             printf("exit\n");
@@ -33,3 +33,96 @@ int main(void)
     }
     return 0;
 }
+
+//////////##TESTE DE TOKENIZAÇÂO##/////////////
+// void print_token_type(t_token_type type)
+// {
+//     switch (type)
+//     {
+//         case TOKEN_WORD: printf("WORD"); break;
+//         case TOKEN_PIPE: printf("PIPE"); break;
+//         case TOKEN_REDIR_IN: printf("REDIR_IN"); break;
+//         case TOKEN_REDIR_OUT: printf("REDIR_OUT"); break;
+//         case TOKEN_APPEND: printf("APPEND"); break;
+//         case TOKEN_QUOTE: printf("QUOTE"); break;
+//         case TOKEN_DQUOTE: printf("DQUOTE"); break;
+//         default: printf("UNKNOWN"); break;
+//     }
+// }
+
+// void print_tokens(t_token *tokens)
+// {
+//     if (!tokens)
+//     {
+//         printf("No tokens to print (NULL)\n");
+//         return;
+//     }
+
+//     t_token *current = tokens;
+//     int i = 0;
+
+//     while (current)
+//     {
+//         if (!current->value)
+//         {
+//             printf("Token %d has NULL value\n", i);
+//             return;
+//         }
+//         printf("Token %d:\n", i++);
+//         printf("  Value: [%s]\n", current->value);
+//         printf("  Type:  ");
+//         print_token_type(current->type);
+//         printf("\n");
+//         current = current->next;
+//     }
+// }
+
+// int main(void)
+// {
+//     char *input;
+
+//     while (1) {
+//         // Obtém a entrada do usuário
+//         input = readline("TBShell> ");
+//         if (!input) {
+//             printf("exit\n");
+//             break;
+//         }
+
+//         // Adiciona a entrada ao histórico (opcional)
+//         add_history(input);
+
+//         // Debug print
+//         printf("\nTesting input: [%s]\n", input);
+//         printf("------------------------\n");
+
+//         // Processa a entrada com tokenização
+//         printf("Calling tokenization_loop...\n");
+//         t_tokenizer *tokenizer = tokenization_loop(input);
+//         if (!tokenizer) {
+//             printf("Error: tokenizer is NULL\n");
+//             free(input); // Libera a memória alocada para a entrada
+//             continue;
+//         }
+
+//         // Debug print
+//         printf("Tokenizer created successfully\n");
+
+//         if (!tokenizer->tokens) {
+//             printf("Warning: No tokens were created (tokenizer->tokens is NULL)\n");
+//         }
+
+//         // Imprime os tokens gerados
+//         print_tokens(tokenizer->tokens);
+
+//         // Libera a memória alocada
+//         printf("Freeing tokens...\n");
+//         free_all_tokens(tokenizer);
+//         free(tokenizer); // Libera o próprio tokenizer após liberar os tokens
+//         free(input);
+
+//         printf("Memory freed successfully\n");
+//     }
+
+//     return 0;
+// }
