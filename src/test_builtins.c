a0c6faa3a23bb8e68a1b2083ad1f8ef246d4594b
Lavinia Saenger <seu_email@dominio.com>
2025-03-17 14:20:28 -0300

    tests biultins

--- src/test_builtins.c
+++ src/test_builtins.c
@@ -6,7 +6,7 @@
 /*   By: lavinia <lavinia@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/10 18:45:47 by lamachad          #+#    #+#             */
-/*   Updated: 2025/03/15 16:19:44 by lavinia          ###   ########.fr       */
+/*   Updated: 2025/03/16 00:23:22 by lavinia          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,28 @@
 
 int	is_builtin(t_cmd_node *cmd)
 {
-	if (!cmd || !cmd->args || !cmd->args[0])
+	const char	*builtins[7];
+	int			i;
+
+	if (!cmd || !cmd->args || !cmd->args[0] || cmd->args[0][0] == '\0')
 		return (0);
-	if (!ft_strcmp(cmd->args[0], "echo") || !ft_strcmp(cmd->args[0], "cd") ||
-		!ft_strcmp(cmd->args[0], "pwd") || !ft_strcmp(cmd->args[0], "export") ||
-		!ft_strcmp(cmd->args[0], "unset") || !ft_strcmp(cmd->args[0], "env") ||
-		!ft_strcmp(cmd->args[0], "exit"))
-		return (1);
+	builtins[0] = "echo";
+	builtins[1] = "cd";
+	builtins[2] = "pwd";
+	builtins[3] = "export";
+	builtins[4] = "unset";
+	builtins[5] = "env";
+	builtins[6] = "exit";
+	i = 0;
+	while (i < 7)
+	{
+		if (!ft_strcmp(cmd->args[0], builtins[i]))
+			return (1);
+		i++;
+	}
 	return (0);
 }
+
 
 void	execute_builtin(t_cmd_node *cmd, t_shell *shell)
 {
@@ -33,10 +46,7 @@ void	execute_builtin(t_cmd_node *cmd, t_shell *shell)
 	else if (ft_strcmp(cmd->args[0], "cd") == 0)
 		builtin_cd(cmd);
 	else if (ft_strcmp(cmd->args[0], "export") == 0)
-	{
-		if (cmd->args[1])
-			builtin_export(shell, cmd->args[1]);
-	}
+		builtin_export(shell, cmd->args[1]);
 	else if (ft_strcmp(cmd->args[0], "unset") == 0)
 	{
 		if (cmd->args[1])
@@ -49,3 +59,4 @@ void	execute_builtin(t_cmd_node *cmd, t_shell *shell)
 	else if (ft_strcmp(cmd->args[0], "env") == 0)
 		builtin_env(shell);
 }
+
